a364e1ae9f762353fbfd21fe0ac9cff4af5bd03d
will <458635496@qq.com>
2024-06-13 16:45:36 +0800

    fix bug and reformat

--- my_tools/coco.py
+++ my_tools/coco.py
@@ -1,9 +1,9 @@
 import json
 from pathlib import Path
-from copy import deepcopy
+
 
 class Coco:
-    def __init__(self,anno_file):
+    def __init__(self, anno_file):
         with open(anno_file) as f:
             data = json.load(f)
 
@@ -20,13 +20,13 @@ class Coco:
     def build_image_annotation_dict(self):
         image_anno_dict = {}
         for x in self._annotations:
-            if x["id"] in image_anno_dict:
-                image_anno_dict[x['id']].append(x)
+            if x["image_id"] in image_anno_dict:
+                image_anno_dict[x['image_id']].append(x)
             else:
-                image_anno_dict[x['id']] = [x,]
+                image_anno_dict[x['image_id']] = [x, ]
 
-        image_dict = {x["id"]:x for x in self._images}
-        cate_dict = {x['id']:x for x in self._categories}
+        image_dict = {x["id"]: x for x in self._images}
+        cate_dict = {x['id']: x for x in self._categories}
 
         self._image_anno_dict = image_anno_dict
         self._image_id_dict = image_dict
@@ -34,22 +34,23 @@ class Coco:
 
     @property
     def images(self):
-        return [(x["id"],x["file_name"]) for x in self._images]
+        return [(x["id"], x["file_name"]) for x in self._images]
+
     @property
     def categories(self):
-        return [(x["id"],x["name"]) for x in self._categories]
+        return [(x["id"], x["name"]) for x in self._categories]
 
     def __repr__(self):
         return f"CoCoData of {self.anno_file}, with image size {len(self.images)} and annotation size {len(self.annotations)}"
 
     def __iter__(self):
-        for im_id,name in self.images:
+        for im_id, name in self.images:
             yield self.sample(im_id)
 
-    def sample(self,id):
-        return self._image_id_dict[id],self._image_anno_dict[id]
+    def sample(self, id):
+        return self._image_id_dict[id], self._image_anno_dict[id] if id in self._image_anno_dict else []
 
-    def update(self,categories=None,images=None,annotations=None):
+    def update(self, categories=None, images=None, annotations=None):
         if categories is not None:
             self._categories = categories
         if images is not None:
--- my_tools/visual.py
+++ my_tools/visual.py
@@ -1,33 +1,34 @@
 import numpy as np
 import cv2
 
-def plot_image(image,bbox=None,pts=None,label=None,color=(0,0,255)):
+
+def plot_image(image, bbox=None, pts=None, label=None, color=(0, 0, 255)):
     image = np.copy(image)
 
     if bbox is not None:
         bbox = np.array(bbox).reshape(4).astype(np.int32)
-        cv2.rectangle(image,bbox[:2],bbox[2:],color=color)
+        cv2.rectangle(image, bbox[:2], bbox[2:], color=color)
 
     if pts is not None:
-        pts= np.array(pts).reshape(-1,2).astype(np.int32)
+        pts = np.array(pts).reshape(-1, 2).astype(np.int32)
         for pt in pts:
-            cv2.circle(image,pt,radius=2,color=color,thickness=-1)
+            cv2.circle(image, pt, radius=2, color=color, thickness=-1)
 
     if label is not None:
-        h, w = image.shape
+        h, w = image.shape[:2]
 
-        x1,y1 = bbox[:2] if bbox is not None else (5,h-5)
-        if isinstance(label,(list,tuple)):
+        x1, y1 = bbox[:2] if bbox is not None else (5, h - 5)
+        if isinstance(label, (list, tuple)):
             text = ";".join(label)
         else:
             text = str(label)
 
-        retval, baseline = cv2.getTextSize(text,cv2.FONT_HERSHEY_SIMPLEX,0.5,2)
-        x1-=2
-        y1-=2
-        topleft = (x1,y1-retval[1])
-        bottomright = (topleft[0]+retval[0],topleft[1]+retval[1])
-        cv2.rectangle(image,(topleft[0],topleft[1]-baseline),bottomright,thickness=-1,color=(0,0,0))
-        cv2.putText(image,text,(x1-2,y1-2),cv2.FONT_HERSHEY_SIMPLEX,0.5,color=color,thickness=2)
+        retval, baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
+        x1 -= 2
+        y1 -= 2
+        topleft = (x1, y1 - retval[1])
+        bottomright = (topleft[0] + retval[0], topleft[1] + retval[1])
+        cv2.rectangle(image, (topleft[0], topleft[1] - baseline), bottomright, thickness=-1, color=(0, 0, 0))
+        cv2.putText(image, text, (x1 - 2, y1 - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color=color, thickness=2)
 
     return image
